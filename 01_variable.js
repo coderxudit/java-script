51ed3eb990bf8b6b75bb6d933825aa34aad800a3
Udit Patel <patelridit676@gmail.com>
2026-03-22 06:21:00 +0000

    container config done

--- 01_variable.js
+++ 01_variable.js
@@ -1,1 +1,20 @@
+const accountId = 144552
+let accountEmail = "udit123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
 
+//accountId = 2 // not allowed 
+
+accountEmail = "hc@gmail.com"
+accountPassword = "212121"
+accountCity = "chennai"
+/*
+const is used to declare the conatant
+let is used to declare variable 
+var is not used because of the problem of scope 
+*/
+
+console.log(accountId)
+
+console.table([accountId, accountEmail, accountPassword, accountCity] )
